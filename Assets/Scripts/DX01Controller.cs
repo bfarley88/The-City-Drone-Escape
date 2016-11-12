408df546513f07b62b079ee72c4fbacf4c750e10
bfarley88 <bfarley88@gmail.com>
2016-11-12 07:31:28 -0800

    TCDE
    DX01

---
+++ Assets/Scripts/DX01Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DX01Controller : MonoBehaviour {
+
+	float speedForce = 40f;
+	float torqueForce = 3f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Update() {
+		// check for button up down here
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+		if(Input.GetButton("AButton")) {
+			rb.AddForce( transform.up * speedForce );
+		}
+
+		rb.AddTorque( Input.GetAxis("LeftJoystickHorizontal") * torqueForce );
+	}
+}
---
+++ Assets/Scripts/DX01aController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DX01aController : MonoBehaviour {
+
+	float speedForce = 22f;
+	float torqueForce = 7f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Update() {
+		// check for button up down here
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+		if(Input.GetButton("AButton")) {
+			rb.AddForce( transform.up * speedForce );
+		}
+
+		rb.AddTorque( Input.GetAxis("LeftJoystickHorizontal") * torqueForce );
+	}
+}
